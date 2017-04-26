7e5a9cca7adedad821a17585d9c28a3208fd1e53
okuyama <k.okuyama@fosbury.co.jp>
2017-04-26 18:33:57 +0000

    BDD プラグインの追加

--- app/Vendor/composer/autoload_real.php
+++ app/Vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit8dee6aca2138f5e8fed57325021c7dd5
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit8dee6aca2138f5e8fed57325021c7dd5', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
@@ -47,6 +51,24 @@ class ComposerAutoloaderInit8dee6aca2138f5e8fed57325021c7dd5
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8dee6aca2138f5e8fed57325021c7dd5::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8dee6aca2138f5e8fed57325021c7dd5($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8dee6aca2138f5e8fed57325021c7dd5($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
