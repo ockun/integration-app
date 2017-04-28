4ede085caa64bfc76b02cbf5acd79fa300660d53
okuyama <k.okuyama@fosbury.co.jp>
2017-04-28 10:33:15 +0000

    postテーブル追加 5.3.2

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -16,7 +16,7 @@
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 require_once dirname(dirname(__FILE__)) . DS . 'Vendor' . DS . 'autoload.php';
-include dirname(__FILE__) . DS . 'bootstrap' . DS . 'Environments.php';
+include dirname(__FILE__) . DS . 'bootstrap' . DS . 'environments.php';
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
  *
